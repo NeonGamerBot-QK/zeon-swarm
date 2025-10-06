469ee33e897e3d859693eac081c5b4bf67ae3337
Neon <neon@saahild.com>
2025-10-06 19:13:27 -0400

    idk gng

--- server/index.js
+++ server/index.js
@@ -1,3 +1,20 @@
 const express = require('express')
 const SocketIo = require('socket-io')
 const io = SocketIo()
+const app = express()
+const http = require('http')
+const server = http.createServer(app)
+
+const PORT = process.env.PORT || 3000
+
+app.use(express.static('public'))
+
+
+
+
+
+io.attach(server)
+
+server.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`)
+})
